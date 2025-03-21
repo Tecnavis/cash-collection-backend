677b43a43ca14108b97692a727b90e7bdbbce454
adhulyamaya <adhulyamaya927@gmail.com>
2025-03-21 15:23:47 +0530

    user conflits

--- api/v1/users_api/views.py
+++ api/v1/users_api/views.py
@@ -17,50 +17,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# def register_user(request):
-#     """
-#     Register Secondary Admin, Customer, or Vendor based on the role passed.
-#     No authentication is required for registration.
-#     """
-#     print(f"Request data: {request.data}")
-    
-#     role_name = request.data.get('role')
-#     print(f"Received role: {role_name}")
-#     valid_roles = ['main_admin','staffs', 'customer', 'vendor']
-#     if role_name not in valid_roles:
-#         return Response({"detail": f"Invalid role specified. Only {', '.join(valid_roles)} are allowed."}, status=status.HTTP_400_BAD_REQUEST)
-
-#     try:
-#         role = Role.objects.get(name=role_name)
-#         request.data['role'] = role.id
-#     except Role.DoesNotExist:
-#         return Response({"detail": f"Role '{role_name}' does not exist in the database."}, status=status.HTTP_400_BAD_REQUEST)
-
-#     serializer = UserRegistrationSerializer(data=request.data)
-
-#     if serializer.is_valid():
-#         user = serializer.save()
-#         refresh = RefreshToken.for_user(user)
-#         access_token = str(refresh.access_token)
-
-#         return Response(
-#             {
-#                 'detail': f'{role_name.capitalize()} registered successfully!',
-#                 'user_id': user.id,
-#                 'access_token': access_token,
-#                 'refresh_token': str(refresh),
-#             },
-#             status=status.HTTP_201_CREATED
-#         )
-#     else:
-#         print(f"Serializer errors: {serializer.errors}")
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login_user(request):
@@ -129,7 +85,7 @@ def create_staff_user(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-@permission_classes([IsMainAdmin])  
+@permission_classes([IsSecondaryAdmin])  
 def create_admin_user(request):
     serializer = CustomUserSerializer(data=request.data)
     
@@ -177,7 +133,7 @@ def update_staff_user(request, id):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
-@permission_classes([IsMainAdmin])  
+@permission_classes([IsSecondaryAdmin | IsMainAdmin])  
 def update_admin_user(request, id):
     try:
         staff_user = CustomUser.objects.get(pk=id)
@@ -202,7 +158,7 @@ def delete_staff_user(request, id):
 
 
 @api_view(['DELETE'])
-@permission_classes([IsMainAdmin])  
+@permission_classes([IsMainAdmin | IsSecondaryAdmin])  
 def delete_admin_user(request, id):
     staff_user = CustomUser.objects.get(pk=id)
     staff_user.delete()
